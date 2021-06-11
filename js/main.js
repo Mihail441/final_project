b75f7a66d49018129c30346fb84606a905cf9e30
Mihail <disakmihail@gmail.com>
2021-06-11 12:24:45 +0300

    5th commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
+$(document).ready(function(){
+    $('.multiple-items').slick({
+        infinite: true,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+});
 $(function() {
 
-   // scrollTop
-   var $page = $('html, body');
-   $('a[href*="#"]').click(function() {
-       $page.animate({
-           scrollTop: $($.attr(this, 'href')).offset().top
-       }, 1800);
-       return false;
-   });
-   
-
-   // Collapse
-   $("[data-collapse]").on("click", function(event) {
-       event.preventDefault();
-
-       var $this = $(this),
-       learning = $this.data('collapse');
-
-       $this.toggleClass("active");
-   });
-
-
-   // /* Slider */
-   // $("[data-slider]").slick({
-   //     infinite: true,
-   //     fade: false,
-   //     slidesToShow: 1,
-   //     slidesToScroll: 1
-   // });
-
+// scrollTop
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1600);
+    return false;
 });
+
+
+// Collapse
+$("[data-collapse]").on("click", function(event) {
+    event.preventDefault();
+
+    var $this = $(this),
+        blockId = $this.data('collapse');
+
+    $this.toggleClass("active");
+})
+})
+var acc = document.getElementsByClassName("accordion");
+var i;
